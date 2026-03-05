a43636b6cd973c78b1e52daad8bcac975ac3b6cb
AbnerRRachan <abmer.rachan@gmail.com>
2026-03-05 19:46:07 -0300

    alteraçoes visuais e de rotas

--- crypto_wallet/app/helpers/application_helper.rb
+++ crypto_wallet/app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def data_br(data_us)
+    data_us.strftime("%d/%m/%Y")  
+  end
+  
+  def name_application
+    "Cripyto Wallet"
+  end
+
+  def set_ambiente
+    if Rails.env.development?
+      "Desenvolvimeno"
+    elsif Rails.env.production?
+      "produçâo"
+    else
+      "teste"
+    end
+  end
+
 end
